1d8106ab68b1b900276acafedead60c08b9f39a6
barcat <przemek.kociecki@gmail.com>
2018-08-30 21:40:28 +0200

    add grid css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <cell shortcut="ctr + b" shortcut-description="some text..."></cell>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Cell from './components/Cell.vue';
 
 @Component({
   components: {
-    // HelloWorld,
+    Cell,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  line-break: 24px;
+  font-size: 16px;  
 }
+
+.container {
+  margin: 0 auto;
+  width: 800px;
+  display: grid;
+  grid-template-columns: 400px 400px;
+}
+
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cell">
+      <h2>{{ shortcut }}</h2>
+      <p>
+        <i>{{ shortcutDescription }}</i>
+      </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  props: {
+    shortcut: String,
+    shortcutDescription: String,
+  },
+})
+
+export default class Cell extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+.cell {
+}
+h2{
+  font-weight: bold;
+  text-align: left;
+  margin-left: 30px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  font-size: 18px
+}
+
+p {
+    text-align: left;
+    margin-left: 20px;
+    margin-bottom: 12px;
+}
+</style>
+
+
+
+
